c4b68c522e4f5e829879171a4cc0879c8e1d34b2
Jakab András <huawei8655@gmail.com>
2012-11-15 10:24:23 +0100

    some changes

--- makerom.sh
+++ makerom.sh
@@ -1,6 +1,12 @@
 #/bin/bash
 alias zipme='zip -4 -r'
 
-dialog --fselect base 20 20 &2>tmp
-dialog --programbox 20 40
+dialog --fselect base/* 20 20 2>tmp
+RES=$?
+echo $RES
+if [ $RES -eq 1 ]; then
+	exit
+fi
+cat tmp
+
 
